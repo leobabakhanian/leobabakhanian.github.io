72c65b0677d06485798b228fec6727d818300899
Leo Babakhanian <babakhanianleo@gmail.com>
2021-11-26 18:51:31 -0800

    Updates

---
+++ precache-manifest.47f4cd98815171f7d19dddee7c93d329.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b562b44faed8437f9f8e9a236d91b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fafaf080cfdf2f85daee",
+    "url": "/static/css/main.65ff7556.chunk.css"
+  },
+  {
+    "revision": "188f94d87591a38e96d6",
+    "url": "/static/js/2.df6401a2.chunk.js"
+  },
+  {
+    "revision": "3ca63f76bf4b9121008457ebb1bd5405",
+    "url": "/static/js/2.df6401a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fafaf080cfdf2f85daee",
+    "url": "/static/js/main.4b729bd7.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  },
+  {
+    "revision": "a8fe5e42472ba4fc849b981f7667196b",
+    "url": "/static/media/apple-clone_1.a8fe5e42.jpg"
+  },
+  {
+    "revision": "1dd2d6f932d53c74c1e732e9dc6c487a",
+    "url": "/static/media/apple-clone_2.1dd2d6f9.jpg"
+  },
+  {
+    "revision": "979098d9e96d64e73b5e443e02ef2dbc",
+    "url": "/static/media/bayc_barca.979098d9.jpg"
+  },
+  {
+    "revision": "824e22b30e390dee687a9e54c871e230",
+    "url": "/static/media/calculator_1.824e22b3.jpg"
+  },
+  {
+    "revision": "ea89da0f1c461bbc167142769700967c",
+    "url": "/static/media/calculator_2.ea89da0f.jpg"
+  },
+  {
+    "revision": "b3f491acf3e317155d198190d95e6fad",
+    "url": "/static/media/google-clone_1.b3f491ac.jpg"
+  },
+  {
+    "revision": "481bfa329253aba3af4b99e986a5708c",
+    "url": "/static/media/google-clone_2.481bfa32.jpg"
+  },
+  {
+    "revision": "d8e03f1540e4457541d6142e4b8cde5b",
+    "url": "/static/media/login_1.d8e03f15.jpg"
+  },
+  {
+    "revision": "0516c96d2303e9b95cb93815497fe7bc",
+    "url": "/static/media/login_2.0516c96d.jpg"
+  },
+  {
+    "revision": "13d71ab9a4872ea6193e672d9be5d6c7",
+    "url": "/static/media/login_3.13d71ab9.jpg"
+  },
+  {
+    "revision": "b01a281ef1be5c1b4f09c0a207f3bdf2",
+    "url": "/static/media/logo-wide.b01a281e.png"
+  },
+  {
+    "revision": "1eff8dca6082c69d281ce3f843aaaec4",
+    "url": "/static/media/spotify-clone_1.1eff8dca.jpg"
+  },
+  {
+    "revision": "90ad4c8a4e7817cc89dfe67ff850dc86",
+    "url": "/static/media/spotify-clone_2.90ad4c8a.jpg"
+  },
+  {
+    "revision": "b4df9f9ac2fc7f3d8fc105e0454931ca",
+    "url": "/static/media/tic-tac-toe_1.b4df9f9a.jpg"
+  },
+  {
+    "revision": "5a3a77aebb9caeefa44742fcf862b029",
+    "url": "/static/media/tic-tac-toe_2.5a3a77ae.jpg"
+  },
+  {
+    "revision": "bd033137ac0d30b8cfe5037278062d71",
+    "url": "/static/media/tic-tac-toe_3.bd033137.jpg"
+  },
+  {
+    "revision": "9a2d7fb023c86a2b377dce94a01b1640",
+    "url": "/static/media/transfermarket_1.9a2d7fb0.jpg"
+  },
+  {
+    "revision": "ba0ebda7f615de2921aba320e9ca093c",
+    "url": "/static/media/transfermarket_2.ba0ebda7.jpg"
+  },
+  {
+    "revision": "354181aa2a49c5c8f0ad27e11de3d9fa",
+    "url": "/static/media/under-construction.354181aa.jpg"
+  },
+  {
+    "revision": "60b5c13f116a3bae2e4adddd1e7b971d",
+    "url": "/static/media/united-states.60b5c13f.png"
+  }
+]);
