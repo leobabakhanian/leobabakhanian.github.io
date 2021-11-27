aea19011adf871dfc797f576e46aaf8efbaf78b8
Leo Babakhanian <babakhanianleo@gmail.com>
2021-11-26 19:47:37 -0800

    fixed links

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import "./App.css";
 import Home from "./components/pages/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Projects from "./components/pages/Projects";
 import More from "./components/pages/More";
 import About from "./components/pages/About";
@@ -18,62 +18,22 @@ import ScientificCalculator from "./components/pages/ScientificCalculator";
 function App() {
   return (
     <>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Header />
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/projects" exact component={Projects} />
+          <Route path="/more" exact component={More} />
+          <Route path="/about" exact component={About} />
+          <Route path="/matapals" exact component={MataPals} />
+          <Route path="/tic-tac-toe" exact component={TicTacToe} />
+          <Route path="/transfermarket" exact component={TransferMarket} />
+          <Route path="/apple-clone" exact component={AppleClone} />
+          <Route path="/spotify-clone" exact component={SpotifyClone} />
+          <Route path="/google-clone" exact component={GoogleClone} />
+          <Route path="/login-system" exact component={LoginSystem} />
           <Route
-            path={process.env.PUBLIC_URL + "/projects"}
-            exact
-            component={Projects}
-          />
-          <Route
-            path={process.env.PUBLIC_URL + "/more"}
-            exact
-            component={More}
-          />
-          <Route
-            path={process.env.PUBLIC_URL + "/about"}
-            exact
-            component={About}
-          />
-          <Route
-            path={process.env.PUBLIC_URL + "/matapals"}
-            exact
-            component={MataPals}
-          />
-          <Route
-            path={process.env.PUBLIC_URL + "/tic-tac-toe"}
-            exact
-            component={TicTacToe}
-          />
-          <Route
-            path={process.env.PUBLIC_URL + "/transfermarket"}
-            exact
-            component={TransferMarket}
-          />
-          <Route
-            path={process.env.PUBLIC_URL + "/apple-clone"}
-            exact
-            component={AppleClone}
-          />
-          <Route
-            path={process.env.PUBLIC_URL + "/spotify-clone"}
-            exact
-            component={SpotifyClone}
-          />
-          <Route
-            path={process.env.PUBLIC_URL + "/google-clone"}
-            exact
-            component={GoogleClone}
-          />
-          <Route
-            path={process.env.PUBLIC_URL + "/login-system"}
-            exact
-            component={LoginSystem}
-          />
-          <Route
-            path={process.env.PUBLIC_URL + "/scientific-calculator"}
+            path="/scientific-calculator"
             exact
             component={ScientificCalculator}
           />
